e59d61de357bc6b0e4a6459a32284adf43e14c58
Maxwell Poffenbarger <maxpoffenbarger@gmail.com>
2020-07-29 14:23:34 -0600

    Added project base file.

---
+++ TaskColton/TaskColton/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TaskColton
+//
+//  Created by Maxwell Poffenbarger on 7/29/20.
+//  Copyright © 2020 Maxwell Poffenbarger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
